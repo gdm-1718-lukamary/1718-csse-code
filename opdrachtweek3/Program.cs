9a369913fd0a1e2619d40b7763da5164e63f9385
gdm-1718-lukamary <lukamary@student.arteveldehs.be>
2017-10-14 18:24:19 +0200

    folder set up

---
+++ opdrachtweek3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace opdrachtweek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine("the rest of the application");
+        }
+
+        private static void ThisIsAFunction()
+        {
+
+        }
+    }
+}
