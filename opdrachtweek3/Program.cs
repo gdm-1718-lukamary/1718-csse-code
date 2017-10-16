1e93c618f2cb4967b3cfc3fb8328fe6dec41732c
gdm-1718-lukamary <lukamary@student.arteveldehs.be>
2017-10-16 16:06:51 +0200

    all functional

--- opdrachtweek3/Program.cs
+++ opdrachtweek3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace opdrachtweek3
 {
@@ -7,14 +8,57 @@ namespace opdrachtweek3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-            Console.WriteLine("the rest of the application");
+            Console.WriteLine("Voer een getal in");
+
+            int getal = Convert.ToInt32(Console.ReadLine());
+
+            //aanmaken array
+            int[] solutionArray = new int[11];
+            //aanmake hashtable
+            Hashtable solutionHashtable = new Hashtable();
+            //aanmaken List
+            var solutionList = new List<int>();
+
+            Console.WriteLine("Vermenigvuldigtal: {0}", getal);
+            Console.WriteLine("Vermenigvuldigingstafel:");
+
+            for (int i = 0; i <= 10; i++) {
+                int result =  i*getal;
+                putResultInArray(solutionArray, solutionHashtable, solutionList, result, i);
+                Console.WriteLine("{0} x {1} = {2} ",i,getal,result);
+            }
+        
+            writeArray(solutionArray);
+            writeHashtable(solutionHashtable);
+            writeList(solutionList);
+           
         }
 
-        private static void ThisIsAFunction()
-        {
+        private static void putResultInArray(int[] solutionArray, Hashtable solutionHashtable,List<int> solutionList, int result, int i) {
+            solutionArray[i] = result;
+            solutionHashtable[i] = result;
+            solutionList.Add(result);
+        }
 
+        private static void writeArray(int[] solutionArray) {
+            Console.WriteLine("Resultaten in een array:");
+            for (int i = 0; i < 11; i++) {
+                Console.WriteLine("{0}",solutionArray[i]);
+            }  
+        }
+
+        private static void writeHashtable(Hashtable solutionHashtable) {
+            Console.WriteLine("Resultaten in een hashtable:");
+            for (int i = 0; i < 11; i++) {
+                Console.WriteLine("{0}",solutionHashtable[i]);
+            }  
+        }
+
+        private static void writeList(List<int> solutionList) {
+            Console.WriteLine("Resultaten in een list:");
+            for (int i = 0; i < 11; i++) {
+                Console.WriteLine("{0}",solutionList[i]);
+            }  
         }
     }
 }
